af39930f0a14081f7177e6b2eafc1f3ea16ced4b
Salma <salmaelaadam@gmail.com>
2023-06-06 12:46:24 +0100

    6.Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for digit in range(0, 8):
+for digit in range(0, 9):
     for duo in range(digit + 1, 10):
         print("{:d}{:d}".format(digit, duo), end=', ')
-    print("{:d}{:d}".format(digit + 1, duo))
+print("{:d}{:d}".format(digit + 1, duo))
