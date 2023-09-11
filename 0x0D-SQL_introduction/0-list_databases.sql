6a626146379bb40d650fa156e2b7cedfecedc4c6
Salma <125903766+Salmael1004@users.noreply.github.com>
2023-09-11 14:40:12 +0100

    Delete 0x0D-SQL_introduction directory

--- 0x0D-SQL_introduction/0-list_databases.sql
+++
@@ -1,2 +0,0 @@
--- script that list all databses of your MySQL server
-SHOW DATABASES;
--- 0x0D-SQL_introduction/8-count_89.sql
+++
@@ -1,2 +0,0 @@
--- Displays the number of records with id = 89 in the table first_table.
-SELECT COUNT(*) FROM first_table WHERE id = 89;
